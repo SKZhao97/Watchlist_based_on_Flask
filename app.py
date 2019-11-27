a98de235444ceedc073afa7d0fe0f2af6f4bb314
Sikai Zhao <sikaizhao1997@gmail.com>
2019-11-27 16:58:07 -0500

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+import os
+import sys
+import click
 from flask import Flask
 from flask import url_for
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+prefix = 'sqlite:////'
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()  # Read user record
+    movies = Movie.query.all()  # Read Movie record
+    return render_template('index.html', user=user, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -25,19 +36,51 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  
     return 'Test page'
 
+class User(db.Model):  # The name of form is user
+    id = db.Column(db.Integer, primary_key=True)  # Primary Key
+    name = db.Column(db.String(20))  # Name
 
 
+class Movie(db.Model):  # The name of form is movie
+    id = db.Column(db.Integer, primary_key=True)  # Primary Key
+    title = db.Column(db.String(60))  # Title
+    year = db.Column(db.String(4))  # Year
 
-name = 'Sky Zhao'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+
+@app.cli.command()  # Register command
+@click.option('--drop', is_flag=True, help='Create after drop.')  # Set
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # Check if flag is inputed
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # Output reminder
+
+
+@app.cli.command()
+def forge():
+    """Generate data."""
+    db.create_all()
+
+    name = 'Sky Zhao'
+    movies = [
+        {'title': 'Laputa - The Castle in the Sky', 'year': '1986'},
+        {'title': 'Farewell My Concubine', 'year': '1993'},
+        {'title': 'Schindler\'s List', 'year': '1993'},
+        {'title': 'Forrest Gump', 'year': '1994'},
+        {'title': 'The Shawshank Redemption', 'year': '1984'},
+        {'title': 'The Legend of 1900', 'year': '1998'},
+        {'title': 'Saving Private Ryan', 'year': '1998'},
+        {'title': 'The Pursuit of Happyness', 'year': '2006'},
+        {'title': 'The Hurt Locker', 'year': '2008'},
+        {'title': 'Dunkirk', 'year': '2017'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
