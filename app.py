36b91924aa35d13fcc302d1012626a06e9154a17
Sikai Zhao <sikaizhao1997@gmail.com>
2019-11-27 22:06:30 -0500

    Add test based on unittest

--- app.py
+++ app.py
@@ -3,26 +3,161 @@ import sys
 import click
 from flask import Flask
 from flask import url_for
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+from flask import render_template
+from flask import request, redirect, flash
+from flask_login import UserMixin
+from flask_login import login_user
+from flask_login import LoginManager
+from flask_login import login_required, logout_user
+from flask_login import login_required, current_user
+from flask_sqlalchemy import SQLAlchemy  # Import export
+from werkzeug.security import generate_password_hash, check_password_hash
+
 prefix = 'sqlite:////'
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
+# login_manager.login_message
+login_manager = LoginManager(app)  # Instantiate export class
+login_manager.login_view = 'login'
 
-@app.route('/')
-@app.route('/index')
-@app.route('/home')
+
+# Functions
+
+
+@login_manager.user_loader
+def load_user(user_id):  # Create user callback function, use user id as parameter
+    user = User.query.get(int(user_id))  # Use id as the Primary key to search related user
+    return user  # return user
+
+
+@app.context_processor
+def inject_user():  
+    user = User.query.first()
+    return dict(user=user)  # return a dict，equals to return {'user': user}
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = User.query.first()  # Read user record
-    movies = Movie.query.all()  # Read Movie record
+    if request.method == 'POST':  # Check if is POST request
+        # Obtain form data
+        if not current_user.is_authenticated:  # if the current user has not been authenticated
+            return redirect(url_for('index'))  # redirect to home
+        title = request.form.get('title')  # name value of input form
+        year = request.form.get('year')
+        # check data
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # Show error
+            return redirect(url_for('index'))  # redirect to home
+        # Save form to database
+        movie = Movie(title=title, year=year)  # Create record
+        db.session.add(movie)  # Add database conversation
+        db.session.commit()  # Submit
+        flash('Item created.')  # Show successful creation
+        return redirect(url_for('index'))  # redirect to home
+
+    user = User.query.first()
+    movies = Movie.query.all()
     return render_template('index.html', user=user, movies=movies)
+
+
+@app.errorhandler(404)  # error code to deal with
+def page_not_found(e):  # the error object as parameter
+    # user = User.query.first()
+    return render_template('404.html'), 404  # return template and status code
+
 
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % name
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # Process the request for form edit
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # redirect to the edit page
+
+        movie.title = title  # update title
+        movie.year = year  # update year
+        db.session.commit()  # submit database conversation
+        flash('Item updated.')
+        return redirect(url_for('index'))  # redirect to home
+
+    return render_template('edit.html', movie=movie)  # input the edited movie record
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # POST required
+@login_required  # protection
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # fetch movie record
+    db.session.delete(movie)  # delete related record
+    db.session.commit()  # database
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # redirect
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        # check if the username and passward valid
+        if username == user.username and user.validate_password(password):
+            login_user(user)  # 登入用户
+            flash('Login success.')
+            return redirect(url_for('index'))  # redirect to home page
+
+        flash('Invalid username or password.')  # if wrong show error message
+        return redirect(url_for('login'))  # redirect to login page
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required  # view protection
+def logout():
+    logout_user()  # logout
+    flash('Goodbye.')
+    return redirect(url_for('index'))  # redirect
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+
+        current_user.name = name
+        # user = User.query.first()
+        # user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
+
 
 @app.route('/test')
 def test_url_for():
@@ -36,15 +171,52 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  
     return 'Test page'
 
-class User(db.Model):  # The name of form is user
+
+# Class
+
+
+class User(db.Model, UserMixin):  # The name of form is user
     id = db.Column(db.Integer, primary_key=True)  # Primary Key
     name = db.Column(db.String(20))  # Name
+    username = db.Column(db.String(20))  # username
+    password_hash = db.Column(db.String(128))  # Hash of password
+
+    def set_password(self, password):  # method to set password, use password as parameter
+        self.password_hash = generate_password_hash(password)  # save generated password to related field
+
+    def validate_password(self, password):  # method to validate the password
+        return check_password_hash(self.password_hash, password)  # return bool
 
 
 class Movie(db.Model):  # The name of form is movie
     id = db.Column(db.Integer, primary_key=True)  # Primary Key
     title = db.Column(db.String(60))  # Title
     year = db.Column(db.String(4))  # Year
+
+
+# Command 
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)  # Set password
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)  # Set password
+        db.session.add(user)
+
+    db.session.commit()  # database
+    click.echo('Done.')
 
 
 @app.cli.command()  # Register command
@@ -68,7 +240,7 @@ def forge():
         {'title': 'Farewell My Concubine', 'year': '1993'},
         {'title': 'Schindler\'s List', 'year': '1993'},
         {'title': 'Forrest Gump', 'year': '1994'},
-        {'title': 'The Shawshank Redemption', 'year': '1984'},
+        {'title': 'The Shawshank Redemption', 'year': '1994'},
         {'title': 'The Legend of 1900', 'year': '1998'},
         {'title': 'Saving Private Ryan', 'year': '1998'},
         {'title': 'The Pursuit of Happyness', 'year': '2006'},
---
+++ test_watchlist.py
@@ -0,0 +1,287 @@
+import unittest
+
+from app import app, db, Movie, User, forge, initdb
+
+class WatchlistTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # Uodate setting
+        app.config.update(
+            TESTING=True,
+            SQLALCHEMY_DATABASE_URI='sqlite:///:memory:'
+        )
+        # create database and tables
+        db.create_all()
+        # create test data, one for user, one for movie items
+        user = User(name='Test', username='test')
+        user.set_password('123')
+        movie = Movie(title='Test Movie Title', year='2019')
+        # use add_all() method to add one or more modules example and input the list
+        db.session.add_all([user, movie])
+        db.session.commit()
+
+        self.client = app.test_client()  # create testing app
+        self.runner = app.test_cli_runner()  # create test command runner
+
+
+    def tearDown(self):
+        db.session.remove()  # clear database conversations
+        db.drop_all()  # delete database tables
+
+
+    # test if the testing examples exist
+    def test_app_exist(self):
+        self.assertIsNotNone(app)
+
+
+    # check if the program is being tested
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['TESTING'])
+
+
+    # test 404 page
+    def test_404_page(self):
+        response = self.client.get('/nothing')  # input target URL
+        data = response.get_data(as_text=True)
+        self.assertIn('Page Not Found - 404', data)
+        self.assertIn('Go Back', data)
+        self.assertEqual(response.status_code, 404)  # check response code
+
+
+    # test home page
+    def test_index_page(self):
+        response = self.client.get('/')
+        data = response.get_data(as_text=True)
+        self.assertIn('Test\'s Watchlist', data)
+        self.assertIn('Test Movie Title', data)
+        self.assertEqual(response.status_code, 200)
+
+
+    # Auxiliary function to login
+    def login(self):
+        self.client.post('/login', data=dict(
+            username='test',
+            password='123'
+        ), follow_redirects=True)
+
+
+    # test item creating
+    def test_create_item(self):
+        self.login()
+
+        # test item creating operation
+        response = self.client.post('/', data=dict(
+            title='New Movie',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item created.', data)
+        self.assertIn('New Movie', data)
+
+        # test item creating operation with blank movie title
+        response = self.client.post('/', data=dict(
+            title='',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item created.', data)
+        self.assertIn('Invalid input.', data)
+
+        # test item creating operation with blank movie year
+        response = self.client.post('/', data=dict(
+            title='New Movie',
+            year=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item created.', data)
+        self.assertIn('Invalid input.', data)
+
+
+    # test updating item
+    def test_update_item(self):
+        self.login()
+
+        # test updating page
+        response = self.client.get('/movie/edit/1')
+        data = response.get_data(as_text=True)
+        self.assertIn('Edit item', data)
+        self.assertIn('Test Movie Title', data)
+        self.assertIn('2019', data)
+
+        # test updating item
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='New Movie Edited',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item updated.', data)
+        self.assertIn('New Movie Edited', data)
+
+        # test item updating operation with blank movie title
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item updated.', data)
+        self.assertIn('Invalid input.', data)
+
+        # test item updating operation with blank year
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='New Movie Edited Again',
+            year=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item updated.', data)
+        self.assertNotIn('New Movie Edited Again', data)
+        self.assertIn('Invalid input.', data)
+
+
+    # test item deleting
+    def test_delete_item(self):
+        self.login()
+
+        response = self.client.post('/movie/delete/1', follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item deleted.', data)
+        self.assertNotIn('Test Movie Title', data)
+
+
+    # test login protection
+    def test_login_protect(self):
+        response = self.client.get('/')
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Logout', data)
+        self.assertNotIn('Settings', data)
+        self.assertNotIn('<form method="post">', data)
+        self.assertNotIn('Delete', data)
+        self.assertNotIn('Edit', data)
+
+    # test login
+    def test_login(self):
+        response = self.client.post('/login', data=dict(
+            username='test',
+            password='123'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Login success.', data)
+        self.assertIn('Logout', data)
+        self.assertIn('Settings', data)
+        self.assertIn('Delete', data)
+        self.assertIn('Edit', data)
+        self.assertIn('<form method="post">', data)
+
+        # test using wrong password to login
+        response = self.client.post('/login', data=dict(
+            username='test',
+            password='456'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid username or password.', data)
+
+        # test using wrong username to login 
+        response = self.client.post('/login', data=dict(
+            username='wrong',
+            password='123'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid username or password.', data)
+
+        # test login with blank username
+        response = self.client.post('/login', data=dict(
+            username='',
+            password='123'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid input.', data)
+
+        # test login with blank password
+        response = self.client.post('/login', data=dict(
+            username='test',
+            password=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid input.', data)
+
+    # test logout
+    def test_logout(self):
+        self.login()
+
+        response = self.client.get('/logout', follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Goodbye.', data)
+        self.assertNotIn('Logout', data)
+        self.assertNotIn('Settings', data)
+        self.assertNotIn('Delete', data)
+        self.assertNotIn('Edit', data)
+        self.assertNotIn('<form method="post">', data)
+
+    # test setting
+    def test_settings(self):
+        self.login()
+
+        # test setting page
+        response = self.client.get('/settings')
+        data = response.get_data(as_text=True)
+        self.assertIn('Settings', data)
+        self.assertIn('Your Name', data)
+
+        # test update setting
+        response = self.client.post('/settings', data=dict(
+            name='Grey Li',
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Settings updated.', data)
+        self.assertIn('Grey Li', data)
+
+        # test update setting with blank name
+        response = self.client.post('/settings', data=dict(
+            name='',
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Settings updated.', data)
+        self.assertIn('Invalid input.', data)
+
+
+    # test virtual data
+    def test_forge_command(self):
+        result = self.runner.invoke(forge)
+        self.assertIn('Done.', result.output)
+        self.assertNotEqual(Movie.query.count(), 0)
+
+
+    # test initiating database
+    def test_initdb_command(self):
+        result = self.runner.invoke(initdb)
+        self.assertIn('Initialized database.', result.output)
+
+
+    # test generating administrater 
+    def test_admin_command(self):
+        db.drop_all()
+        db.create_all()
+        result = self.runner.invoke(args=['admin', '--username', 'grey', '--password', '123'])
+        self.assertIn('Creating user...', result.output)
+        self.assertIn('Done.', result.output)
+        self.assertEqual(User.query.count(), 1)
+        self.assertEqual(User.query.first().username, 'grey')
+        self.assertTrue(User.query.first().validate_password('123'))
+
+
+    # test updating admin
+    def test_admin_command_update(self):
+        # obtain complete command parameter list using args 
+        result = self.runner.invoke(args=['admin', '--username', 'peter', '--password', '456'])
+        self.assertIn('Updating user...', result.output)
+        self.assertIn('Done.', result.output)
+        self.assertEqual(User.query.count(), 1)
+        self.assertEqual(User.query.first().username, 'peter')
+        self.assertTrue(User.query.first().validate_password('456'))
+
+
+if __name__ == '__main__':
+    unittest.main()
