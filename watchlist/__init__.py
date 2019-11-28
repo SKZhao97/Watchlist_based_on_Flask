3bb04a5eb9600eb856268b13d1234431d33d4bb1
Sikai Zhao <sikaizhao1997@gmail.com>
2019-11-27 22:51:01 -0500

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -8,9 +8,11 @@ prefix = 'sqlite:////'
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+
+
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)  # Instantiate export class
